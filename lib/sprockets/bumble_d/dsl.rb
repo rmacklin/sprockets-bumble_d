3f83c31eae187347f1347ce31328bafa369c1938
Richard Macklin <richard.github@nrm.com>
2016-06-30 18:29:51 -0700

    Add configure_sprockets_bumble_d DSL method
    This method is just a wrapper around Config#configure that is declared
    to run prior to the es6 transformer initialization, so it will guarantee
    that the config is set before it's too late

--- lib/sprockets/bumble_d/dsl.rb
+++ lib/sprockets/bumble_d/dsl.rb
@@ -1,6 +1,13 @@
 module Sprockets
   module BumbleD
     module DSL
+      def configure_sprockets_bumble_d(&block)
+        initializer 'configure_sprockets_bumble_d',
+                    before: 'sprockets-bumble_d.configure_transformer' do |app|
+          app.config.sprockets_bumble_d.configure(&block)
+        end
+      end
+
       def register_umd_globals(module_name, globals_map)
         initializer "#{module_name}.register_umd_globals",
                     before: 'sprockets-bumble_d.configure_transformer' do |app|
