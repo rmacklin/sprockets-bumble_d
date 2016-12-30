96ae1f2e77d63a8ec5b8f49a27d4d6932ab45c86
Richard Macklin <richard.github@nrm.com>
2016-12-30 00:47:43 -0800

    Remove asset compression in production env in test app

--- test/test_apps/4.2/config/environments/production.rb
+++ test/test_apps/4.2/config/environments/production.rb
@@ -24,10 +24,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
