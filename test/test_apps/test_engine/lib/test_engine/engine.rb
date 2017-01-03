cc47c61d072a243ce844ff6c6892c126880ced7b
Richard Macklin <richard.github@nrm.com>
2017-01-02 23:26:20 -0800

    Register globals for sample ES6 modules in test engine

--- test/test_apps/test_engine/lib/test_engine/engine.rb
+++ test/test_apps/test_engine/lib/test_engine/engine.rb
@@ -1,5 +1,11 @@
 module TestEngine
   class Engine < ::Rails::Engine
     isolate_namespace TestEngine
+
+    extend Sprockets::BumbleD::DSL
+
+    register_umd_globals :test_engine,
+      'test_engine/baz' => 'TestEngine.Baz',
+      'test_engine/qux' => 'TestEngine.Qux'
   end
 end
